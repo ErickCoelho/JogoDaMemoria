9de733e8f55010050498925a4b9c44138c7432c3
Erick Coelho <erickcoelho06@gmail.com>
2023-06-21 13:05:26 -0300

    rotate card animation

--- js/scripts.js
+++ js/scripts.js
@@ -18,46 +18,60 @@ let gameCards = [];
 function distributeCards(){
     cardsQuant = prompt("Com quantas cartas deseja jogar?\n(escolha um valor par entre 4 e 14)");
     const cardsBox = document.querySelector(".cardsbox");
-    
-    for(let i = 0; i < cardsQuant; i++){
-        cardsBox.innerHTML += `<div class="card ${i}" onclick="openCard(this)"><img class="cardimage" src="assets/front.png"></div>`;
-    }
 
     gameCards = cardsArray.slice(0,cardsQuant/2);
     gameCards = gameCards.concat(gameCards).sort(comparador);
+    
+    for(let i = 0; i < cardsQuant; i++){
+        cardsBox.innerHTML += ` <div class="card">\n
+                                    <div class="front-face face" onclick="openCard(this)">\n
+                                        <img class="cardimage" src="assets/front.png">\n
+                                    </div>\n
+                                    <div class="back-face face">\n
+                                        <img class="cardimage" src="assets/${gameCards[i]}">\n
+                                    </div>\n
+                                </div>\n`;
+    }
+
 }
 
 //alert(gameCards);
 
 function turnDownCards(){
-    cardOpened1.innerHTML = `<img class="cardimage" src="assets/front.png">`;
-    cardOpened2.innerHTML = `<img class="cardimage" src="assets/front.png">`;
+    //alert("Turn Down");
+    //cardOpened1.classList.add("TESTEMTOLOKO");
+    //cardOpened2.classList.add("TESTEMTOLOKO");
+    cardOpened1.parentNode.classList.remove("open"); //cardOpened1.innerHTML = `<img class="cardimage" src="assets/front.png">`;
+    cardOpened2.parentNode.classList.remove("open"); //cardOpened2.innerHTML = `<img class="cardimage" src="assets/front.png">`;
 }
 
 
 
 function checkPair(){
-    if(cardOpened1.innerHTML !== cardOpened2.innerHTML){
+    //alert("Check Pair");
+    if(cardOpened1.parentNode.innerHTML !== cardOpened2.parentNode.innerHTML){
+        //alert("if");
         setTimeout(turnDownCards,1000);
     }
     else{
         cardsCheckQuantity += 2;
+        //alert("ELSE");
         if(cardsCheckQuantity == cardsQuant) alert("YOU WIN!!!");
     }
-
+    
     console.log("check pais");
     //if(cardsCheckQuantity === cardsQuant) alert("Fim de Jogo!");
 
 }
 
 function openCard(elemento){
-    if(elemento.innerHTML == `<img class="cardimage" src="assets/front.png">`){
+    if(!elemento.parentNode.classList.contains('open')){// == `<img class="cardimage" src="assets/front.png">`){ //check if classlist contem .open
 
         const test = elemento.classList.toString();
         //alert(test[test.length - 1]);
         const num = parseInt(test[test.length - 1]);
         //alert(gameCards[num]);
-        elemento.innerHTML = `<img class="cardimage turned" src="assets/${gameCards[num]}">`;
+        elemento.parentNode.classList.add("open");//elemento.innerHTML = `<img class="cardimage turned" src="assets/${gameCards[num]}">`;
 
         
         console.log(elemento);

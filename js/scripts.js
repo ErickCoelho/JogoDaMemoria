85bf746e3cf18590966f771fbc0bef2206fdb0e3
Erick Coelho <erickcoelho06@gmail.com>
2023-06-21 10:50:38 -0300

    card match

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 let cardsArray = ['bobrossparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif', 'metalparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif', 'unicornparrot.gif'];
+let cardOpened1;
+let cardOpened2;
+let cardsCheckQuantity = 0;
+let click = 1;
+
 
 function comparador(){
     return Math.random() - 0.5;
@@ -14,13 +19,58 @@ gameCards = gameCards.concat(gameCards).sort(comparador);
 //alert(gameCards);
 const cardsBox = document.querySelector(".cardsbox");
 for(let i = 0; i < cardsQuant; i++){
-    cardsBox.innerHTML += `<div class="card ${i}" onclick="openCard(this)"><img class="cardimage" src="assets/front.png"></div>`
+    cardsBox.innerHTML += `<div class="card ${i}" onclick="openCard(this)"><img class="cardimage" src="assets/front.png"></div>`;
+}
+
+
+function checkpair(){
+    if(cardOpened1.innerHTML !== cardOpened2.innerHTML){
+        cardOpened1.innerHTML = `<img class="cardimage" src="assets/front.png">`;
+        cardOpened2.innerHTML = `<img class="cardimage" src="assets/front.png">`;
+    }
+    else{
+        cardsCheckQuantity += 2;
+    }
+
+    //if(cardsCheckQuantity === cardsQuant) alert("Fim de Jogo!");
+
 }
 
 function openCard(elemento){
-    const test = elemento.classList.toString();
-    //alert(test[test.length - 1]);
-    const num = parseInt(test[test.length - 1]);
-    alert(gameCards[num]);
-    elemento.innerHTML = `<img class="cardimage turned" src="assets/${gameCards[num]}">`
+    if(elemento.innerHTML == `<img class="cardimage" src="assets/front.png">`){
+
+        const test = elemento.classList.toString();
+        //alert(test[test.length - 1]);
+        const num = parseInt(test[test.length - 1]);
+        //alert(gameCards[num]);
+        elemento.innerHTML = `<img class="cardimage turned" src="assets/${gameCards[num]}">`;
+
+        
+        console.log(elemento);
+
+        if(click == 1) {
+            cardOpened1 = elemento;
+            console.log("cardOpened1 = " + cardOpened1.innerHTML);
+            console.log("cardOpened2 = " + "Empty");
+            click++;
+        }
+        else {
+            cardOpened2 = elemento;
+            click--;
+            console.log("cardOpened1 = " + cardOpened1.innerHTML);
+            console.log("cardOpened2 = " + cardOpened2.innerHTML);
+            setTimeout(checkpair,1000);
+                /*if(cardOpened1.innerHTML !== cardOpened2.innerHTML){
+                    cardOpened1.innerHTML = `<img class="cardimage" src="assets/front.png">`;
+                    cardOpened2.innerHTML = `<img class="cardimage" src="assets/front.png">`;
+                }
+                else{
+                    cardsCheckQuantity += 2;
+                }*/
+        }
+        console.log("elemento = " + elemento.innerHTML);
+        //alert(cardOpened1.innerHTML + cardOpened2.innerHTML);
+    }
 }
+
+

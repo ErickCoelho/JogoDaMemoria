e48c0e932a83bc8d948f601902ea622452194cc1
Erick Coelho <erickcoelho06@gmail.com>
2023-06-21 09:17:31 -0300

    print random cards quantity

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-// como fazer um timer em js?
+let cardsArray = ['bobrossparrot.gif', 'explodyparrot.gif', 'fiestaparrot.gif', 'metalparrot.gif', 'revertitparrot.gif', 'tripletsparrot.gif', 'unicornparrot.gif'];
+
+function comparador(){
+    return Math.random() - 0.5;
+}
+
+cardsArray.sort(comparador);
+
+let cardsQuant = 8;
+
+let gameCards = cardsArray.slice(0,cardsQuant/2);
+gameCards = gameCards.concat(gameCards).sort(comparador);
+
+//alert(gameCards);
+const cardsBox = document.querySelector(".cardsbox");
+for(let i = 0; i < cardsQuant; i++){
+    cardsBox.innerHTML += `<div class="card ${i}" onclick="openCard(this)"><img class="cardimage" src="assets/front.png"></div>`
+}
+
+function openCard(){
+
+}
